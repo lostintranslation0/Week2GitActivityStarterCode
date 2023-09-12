1ac67c4121f6b13b636dbec71fc9f25b80df19bb
Dylan McNulty <mcnulty.d@northeastern.edu>
2023-09-12 11:15:40 -0400

    add subtract operation

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ const calc = new Calculator();
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 
+const result_sub = calc.subtract(7, 4);
+console.log(`result_sub = ${result_sub}`);
+

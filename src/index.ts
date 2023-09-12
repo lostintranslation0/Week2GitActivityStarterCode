40aa26d1c24fbb1e1ee350aed80ac5f92668fcf2
Dylan McNulty <mcnulty.d@northeastern.edu>
2023-09-12 12:32:02 -0400

    implement multiply operation

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,6 @@ console.log(`result_add = ${result_add}`);
 const result_sub = calc.subtract(7, 4);
 console.log(`result_sub = ${result_sub}`);
 
+const result_mult = calc.multiply(5, 4);
+console.log(`result_mult = ${result_mult}`);
+

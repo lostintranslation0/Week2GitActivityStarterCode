33497aefecd4c02f797c67022810790c46c8ae5b
usaydsheikh <sheikh.u@northeastern.edu>
2023-09-13 16:16:44 -0400

    implement divide operation
    rebased changes

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,7 +2,13 @@ export class Calculator {
   public add(x: number, y: number) : number {
     return x + y;
   }
+<<<<<<< HEAD
   public subtract(x:number, y: number) : number {
     return x - y;
+=======
+
+  public divide(x: number, y: number) : number {
+    return x / y;
+>>>>>>> f7e6307 (implement divide operation)
   }
 };

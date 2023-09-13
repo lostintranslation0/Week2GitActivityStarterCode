c7eedcd5639dcadae8e8e264c0ff31f76d301c15
usaydsheikh <sheikh.u@northeastern.edu>
2023-09-13 17:29:34 -0400

    fix src

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,13 +2,11 @@ export class Calculator {
   public add(x: number, y: number) : number {
     return x + y;
   }
-<<<<<<< HEAD
+
   public subtract(x:number, y: number) : number {
     return x - y;
-=======
-
+  }
   public divide(x: number, y: number) : number {
     return x / y;
->>>>>>> f7e6307 (implement divide operation)
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const calc = new Calculator();
 const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 
-const result_sub = calc.subtract(7, 4);
+const result_sub = calc.subtract(2, 3);
 console.log(`result_sub = ${result_sub}`);
+
+const result_divide = calc.divide(7, 4);
+console.log(`result_divide = ${result_divide}`);
 
